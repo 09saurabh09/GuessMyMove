6c5104591ad5b50d61188e3910fc69c8e25d4d3f
Vikas <imyselfvikas@gmail.com>
2016-02-14 02:52:27 +0530

    Format and Standards

--- routes/router.js
+++ routes/router.js
@@ -1,3 +1,4 @@
+/*global homeController,userController*/
 var express = require('express');
 var passport = require('passport');
 var router = express.Router();
@@ -5,35 +6,30 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   homeController.home(req, res, next);
-
 });
 
 router.post('/api/users/signup', function(req, res, next) {
-  userController.create(req,res);
-
+  userController.create(req, res);
 });
 
 router.post('/api/users/signin', function(req, res, next) {
-  userController.login(req,res);
-
+  userController.login(req, res);
 });
 
-//router.get('/auth/facebook', function(req, res, next) {
+// router.get('/auth/facebook', function(req, res, next) {
 //  userController.facebookLogin(req,res);
-//
-//});
+// });
 
 router.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
-//
-//router.get('/auth/facebook/callback',
+
+// router.get('/auth/facebook/callback',
 //    passport.authenticate('facebook', {
 //      successRedirect : '/profile',
 //      failureRedirect : '/'
 //    }));
 
 router.get('/auth/facebook/callback', function(req, res, next) {
-  userController.facebookLoginCallback(req,res);
-
+  userController.facebookLoginCallback(req, res);
 });
 
 module.exports = router;
