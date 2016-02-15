563f25fc5450fa864c94a086662254cdd56ee1a4
Vikas <imyselfvikas@gmail.com>
2016-02-15 22:03:03 +0530

    Format and Standards

--- app.js
+++ app.js
@@ -20,16 +20,16 @@ var configDB = require('./config/database.js');
 
 require('./config/wiring.js'); // For making models and controllers globally accessible
 
-require('./config/passport')(passport);  // pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
 require('./config/socketEvents')(io);
 
 // configuration ===============================================================
-mongoose.connect(configDB.mongoUrl, function (err) {
-  if (err) {
-    console.log('Error connecting to mongo');
-  } else {
-    console.log('connected to MongoDB');
-  }
+mongoose.connect(configDB.mongoUrl, function(err) {
+    if (err) {
+        console.log('Error connecting to mongo');
+    } else {
+        console.log('connected to MongoDB');
+    }
 }); // connect to our database
 
 
@@ -39,14 +39,17 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-var sessionMiddleware = session({ store: new RedisStore({
-  host: configDB.redisHost,
-  port: configDB.redisPort,
-  pass: configDB.redisPassword
-}), secret: 'ilovescotchscotchyscotchscotch' });
+var sessionMiddleware = session({
+    store: new RedisStore({
+        host: configDB.redisHost,
+        port: configDB.redisPort,
+        pass: configDB.redisPassword
+    }),
+    secret: 'ilovescotchscotchyscotchscotch'
+});
 
-io.use(function (socket, next) {
-  sessionMiddleware(socket.request, socket.request.res, next);
+io.use(function(socket, next) {
+    sessionMiddleware(socket.request, socket.request.res, next);
 });
 
 app.use(sessionMiddleware);
@@ -55,7 +58,9 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use('/public', express.static(path.join(__dirname, '/public')));
 app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')));
@@ -63,10 +68,10 @@ app.use('/bower_components', express.static(path.join(__dirname, '/bower_compone
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -74,30 +79,30 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
-server.listen(8000, function (err) {
-  if (err) {
-    console.log('Error in starting server');
-  } else {
-    console.log('Server started on port 8000');
-  }
+server.listen(8000, function(err) {
+    if (err) {
+        console.log('Error in starting server');
+    } else {
+        console.log('Server started on port 8000');
+    }
 });
 module.exports = app;
