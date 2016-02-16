6a019a7a23fc66fdefd195349b9f2169fbbe47a3
Vikas <imyselfvikas@gmail.com>
2016-02-17 00:52:50 +0530

    Format and Standards

--- app/models/gameModel.js
+++ app/models/gameModel.js
@@ -3,15 +3,21 @@
  */
 var mongoose = require('mongoose');
 var gameSchema = mongoose.Schema({
-    playerOneEmail : String,
-    playerTwoEmail : String,
-    playerOneId : String,
-    playerTwoId : String,
-    gameId : { type : String, unique: true, index: true},
+    playerOneEmail: String,
+    playerTwoEmail: String,
+    playerOneId: String,
+    playerTwoId: String,
+    gameId: {
+        type: String,
+        unique: true,
+        index: true
+    },
     gameType: Number,
-    isTemporary : Boolean,
-    state : String,
-    winner : String
-}, { timestamps: true });
+    isTemporary: Boolean,
+    state: String,
+    winner: String
+}, {
+    timestamps: true
+});
 
 module.exports = mongoose.model('game', gameSchema);
