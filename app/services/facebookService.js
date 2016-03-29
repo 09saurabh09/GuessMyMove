a9714947f219ec02d199b43f2d9c1cd69fcdff04
Saurabh Kumar <saurabhk@olacabs.com>
2016-03-30 02:43:56 +0530

    Changed get friends function

--- app/services/facebookService.js
+++ app/services/facebookService.js
@@ -6,31 +6,33 @@ var request = require('request');
 var mongoose = require('mongoose');
 var graphAPIHost = 'https://graph.facebook.com';
 module.exports = {
-    getAllFriends: function(token) {
+    getAllFriends: function(token, callback) {
         request(graphAPIHost + '/me/friends?access_token=' + token, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 var friendsList = JSON.parse(body).data;
-                return friendsList.map(function(friend) {return friend.id});
+                callback(friendsList.map(function(friend) {return friend.id}));
             }
         })
     },
 
     addFriends: function(token, user) {
-        var friendIds = this.getAllFriends(token);
+        this.getAllFriends(token, function(friendIds) {
+            UserModel.find()
+                .where("facebook.id")
+                .in(friendIds)
+                .select('_id')
+                .exec(function(err, records) {
+                    if (err) return null;
+                    var newFriends = records.map(function(friend) {return friend.id});
+                    user.friends = user.friends.concat(newFriends);
 
-        UserModel.find()
-            .where("facebook.id")
-            .in(friendIds)
-            .select('_id')
-            .exec(function(err, records) {
-                if (err) return null;
-                var newFriends = records.map(function(friend) {return friend.id});
-                console.log(user.friends, newFriends);
-                user.friends = user.friends.concat(newFriends);
-                user.save(function(err) {
-                    if (err) console.log(err.message);
-                });
-            })
+                    user.save(function(err) {
+                        if (err) console.log(err.message);
+                        console.log("Success ::: Friends added for user: "+user.local.email);
+                    });
+                })
+        });
+
     }
 
 };
